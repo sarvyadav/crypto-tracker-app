ec9d507c65d070aa3201734eb9d70ff9568c40d2
sarv yadav <sarv@sarvs-MacBook-Air.local>
2022-07-14 22:58:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios' //for fetching apis
 import './App.css';
+import Coin from './coin';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState([]);
+  useEffect(() =>{
+    axios
+    .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res =>{
+      setCoins(res.data);
+    })
+    .catch(error => console.log(error));
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  }
+
+  const filteredCoins = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toString().toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a Currency</h1> 
+           <form>
+             <input 
+              placeholder="Search" 
+              type="text"
+              className="coin-input"
+              onChange={handleChange}
+             />
+           </form>
+      </div>
+      {filteredCoins.map(coin => {
+          return (
+          <Coin
+            key={coin.id}
+            image={coin.image}
+            name={coin.name}
+            symbol={coin.symbol}
+            price={coin.current_price}
+            volume={coin.total_volume}
+            priceChange={coin.price_change_percentage_24h}
+            marketCap={coin.market_cap}
+          />
+          );
+      })}
     </div>
   );
 }
